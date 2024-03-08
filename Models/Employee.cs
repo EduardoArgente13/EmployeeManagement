6709ff37ff65bcb69b6cfd289eac54db4f78d096
Eduardo Argente <eduardoargente.dev@gmail.com>
2024-03-08 11:33:31 -0600

    Inital Commit

---
+++ Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Xrm.Sdk.Metadata;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagement
+{
+    [Index(nameof(Employee.RFC), IsUnique = true)]
+
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string LastName { get; set; }
+
+        [RegularExpression("^[A-Z]{4}\\d{6}[A-Z0-9]{3}$")]
+        [Alternatekey]
+        public required string RFC { get; set; }
+
+        public DateTime BornDate { get; set; }
+
+        public EmployeeStatus Status { get; set; }
+    }
+
+    public enum EmployeeStatus
+    {
+        NotSet,
+        Active,
+        Inactive,
+    }
+}
+
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+
+namespace EmployeeManagement
+{
+    public class EmployeeService
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeService(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateEmployee(Employee employee)
+        {
+            try
+            {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                Console.WriteLine("Empleado creado con éxito.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al crear el empleado: " + ex.Message);
+            }
+        }
+
+        public List<Employee> GetEmployeesByName()
+        {
+            var query = _context.Employees
+                .Where(e => e.Status == EmployeeStatus.Active)
+                .OrderBy(e => e.Name)
+                .ThenByDescending(e => e.BornDate);
+
+            return query.ToList();
+        }
+
+        public List<Employee> GetEmployeesByBornDate()
+        {
+            var query = _context.Employees
+                .Where(e => e.Status == EmployeeStatus.Active)
+                .OrderByDescending(e => e.BornDate)
+                .ThenBy(e => e.Name);
+
+            return query.ToList();
+        }
+    }
+}
+
